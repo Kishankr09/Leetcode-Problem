ef69c81ee9a275e43bf2e87c4cec30d1d527fcd5
Kishan Kumar <68832175+Kishankr09@users.noreply.github.com>
2023-11-13 21:24:08 +0530

    Create Check_if_Array_Is_Sorted_and_Rotated.java

---
+++ Check_if_Array_Is_Sorted_and_Rotated.java
@@ -0,0 +1,22 @@
+//1752. Check if the Array Is Sorted and Rotated
+// Leetcode
+
+
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length-1; i++){
+            if(nums[i] > nums[i+1])
+                count++;
+        }
+
+        if(count == 0)
+            return true;
+        else if(count > 1)
+            return false;
+        else if(nums[0] >= nums[nums.length-1])
+                return true;
+             else 
+                return false;
+    }
+}
